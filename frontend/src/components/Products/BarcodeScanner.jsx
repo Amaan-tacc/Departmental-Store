6494f5f124e455dd1924d6cbfb95dc5c10fd4555
Amaan-tacc <hafizamaan18@gmail.com>
2026-04-08 06:18:51 +0500

    Final Edits

--- frontend/src/components/Products/BarcodeScanner.jsx
+++ frontend/src/components/Products/BarcodeScanner.jsx
@@ -64,13 +64,23 @@ const BarcodeScanner = ({ onClose, onScan }) => {
       scannerRef.current = html5QrCode;
 
       const formatsToSupport = [
+        Html5QrcodeSupportedFormats.QR_CODE,
+        Html5QrcodeSupportedFormats.AZTEC,
+        Html5QrcodeSupportedFormats.CODABAR,
+        Html5QrcodeSupportedFormats.CODE_39,
+        Html5QrcodeSupportedFormats.CODE_93,
+        Html5QrcodeSupportedFormats.CODE_128,
+        Html5QrcodeSupportedFormats.DATA_MATRIX,
+        Html5QrcodeSupportedFormats.MAXICODE,
+        Html5QrcodeSupportedFormats.ITF,
         Html5QrcodeSupportedFormats.EAN_13,
         Html5QrcodeSupportedFormats.EAN_8,
-        Html5QrcodeSupportedFormats.CODE_128,
-        Html5QrcodeSupportedFormats.CODE_39,
+        Html5QrcodeSupportedFormats.PDF_417,
+        Html5QrcodeSupportedFormats.RSS_14,
+        Html5QrcodeSupportedFormats.RSS_EXPANDED,
         Html5QrcodeSupportedFormats.UPC_A,
         Html5QrcodeSupportedFormats.UPC_E,
-        Html5QrcodeSupportedFormats.QR_CODE
+        Html5QrcodeSupportedFormats.UPC_EAN_EXTENSION
       ];
 
       const config = {
